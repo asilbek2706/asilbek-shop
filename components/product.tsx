814d633ff83ac2d5dbaa31d83b30fd6022f53d76
Asilbek2706 <249135167+Asilbek2706@users.noreply.github.com>
2026-04-08 17:29:08 +0500

    image optimization

--- components/product.tsx
+++ components/product.tsx
@@ -2,27 +2,29 @@
 
 import { ProductType } from '@/interfaces';
 import { FC } from 'react';
+import Link from 'next/link';
+import CustomImage from './image';
 
 const Product: FC<{ product: ProductType }> = ({ product }) => {
-  return (
-    <div className="bg-gray-100 p-6 rounded-lg">
-      <img
-        className="h-40 rounded w-full object-cover object-center mb-6"
-        src={product.image}
-        alt={product.title}
-      />
-      <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">
-        {product.category}
-      </h3>
-      <div className="font-semibold flex items-center justify-between mt-4 mb-1">
-        <p className="w-44 truncate">{product.title}</p>
-        <p>${product.price}</p>
-      </div>
-      <p className="leading-relaxed text-base line-clamp-2">
-        {product.description}
-      </p>
-    </div>
-  );
+    return (
+        <Link
+            href={`/product/${product.id}`}
+            className='h-96 flex flex-col p-6 rounded-lg group hover:scale-105 transition-transform ease-out duration-200 border'
+        >
+            <div className='relative max-h-80 flex-1'>
+                <CustomImage product={product} fill />
+            </div>
+
+            <h3 className='tracking-widest text-indigo-500 text-xs font-medium title-font mt-5'>
+                {product.category}
+            </h3>
+            <div className='font-semibold flex items-center justify-between mt-4 mb-1'>
+                <p className='w-44 truncate'>{product.title}</p>
+                <p>${product.price}</p>
+            </div>
+            <p className='leading-relaxed text-base line-clamp-2'>{product.description}</p>
+        </Link>
+    );
 };
 
 export default Product;
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'fakestoreapi.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'dummyimage.com',
+            },
+        ],
+    },
 };
 
 export default nextConfig;
